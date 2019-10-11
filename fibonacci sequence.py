1a0f1133c316895f6151076b3d859e2aff0ee196
moonauto <45088862+moonauto@users.noreply.github.com>
2019-10-12 00:21:18 +0800

    Update fibonacci sequence.py

--- fibonacci sequence.py
+++ fibonacci sequence.py
@@ -6,3 +6,12 @@ def fibonacci (max):
         a, b = b, a+b
         n = n + 1
     return 'done'
+
+
+def fibonacci (max):
+    n, a, b =0, 0, 1
+    while n < max:
+        yield b#print(b)
+        a, b = b, a+b
+        n = n + 1
+    return 'done'
